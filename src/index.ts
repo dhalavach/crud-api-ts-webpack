d746572939dfc70a9d2ddd57bb884af47f183461
Dzmitry Halavach <kotik1@protonmail.com>
2023-07-03 23:15:06 +0300

    fix: dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import process from 'process';
+// import process from 'process';
 //@ts-ignore
 import { server } from './server.ts';
 //@ts-ignore
 import { balancer } from './load-balancer/balancer.ts';
 //@ts-ignore
 import { parseArg } from './helpers.ts';
-// import 'dotenv/config.js';
-
+import { config } from 'dotenv';
+config();
 const multithreaded = parseArg('multi');
 const port = parseInt(process.env.PORT) || 5000;
 
